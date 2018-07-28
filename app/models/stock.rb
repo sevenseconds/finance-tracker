341be6a330f547096d32e4ff7ffe3747efcce639
sevenseconds <sevenseconds@gmail.com>
2018-07-28 19:46:35 +0700

    Add user and stock association and views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.new_from_lookup(symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(symbol)
