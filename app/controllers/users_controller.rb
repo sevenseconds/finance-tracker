2896be025648bf8b3231aee50d028440abaa2794
sevenseconds <sevenseconds@gmail.com>
2018-07-29 00:39:10 +0700

    Add user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,4 +32,9 @@ class UsersController < ApplicationController
 
     redirect_to my_friends_path
   end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 end
