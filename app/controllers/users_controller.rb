0b1d59c8de4523c87eca9eff3e7912ae324fea4d
sevenseconds <sevenseconds@gmail.com>
2018-07-29 00:21:15 +0700

    Add and remove friends features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,33 @@ class UsersController < ApplicationController
     @user = current_user
     @user_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friendships = current_user.friends
+  end
+
+  def search
+    if params[:q].blank?
+      flash.now[:danger] = 'You have entered an empty search string'
+    else
+      @users = User.search(params[:q])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = 'No users match' if @users.blank?
+    end
+    respond_to do |format|
+      format.js {render partial: 'friends/result'}
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      flash[:notice] = 'Friend was successfully added'
+    else
+      flash[:danger] = 'There was something wrong with the friend request'
+    end
+
+    redirect_to my_friends_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     'Anonymous'
@@ -25,4 +28,39 @@ class User < ApplicationRecord
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == id }
+  end
+
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) +
+        last_name_matches(param) +
+        email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
 end
